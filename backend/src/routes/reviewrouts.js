eaa49f43b3acc169af1e82a14a343995eaf1f8a5
Rupasinghe W A R K <it22106360@my.sliit.lk>
2024-06-14 17:19:22 +0530

    API change

--- backend/src/routes/reviewrouts.js
+++ backend/src/routes/reviewrouts.js
@@ -1,8 +1,7 @@
 // routes/reviews.js
 const express = require('express');
 const router = express.Router();
-const Review = require('../models/Review');
-const User = require('../models/User');
+const Review = require('../models/review');
 
 // POST a new review
 router.post('/add', async (req, res) => {
@@ -31,24 +30,37 @@ router.post('/add', async (req, res) => {
   }
 });
 
-// GET reviews for a user
-router.get('/:userId', async (req, res) => {
-  try {
-    const reviews = await Review.find({ reviewee: req.params.userId }).populate('reviewer');
-    res.status(200).json(reviews);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+// // GET reviews for a user
+// router.get('/:userId', async (req, res) => {
+//   try {
+//     const reviews = await Review.find({ reviewee: req.params.userId }).populate('reviewer');
+//     res.status(200).json(reviews);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
 
-// GET marketplace reviews
-router.get('/marketplace', async (req, res) => {
-  try {
-    const reviews = await Review.find({ marketplaceReview: true }).populate('reviewer');
-    res.status(200).json(reviews);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+// // GET marketplace reviews
+// router.get('/marketplace', async (req, res) => {
+//   try {
+//     const reviews = await Review.find({ marketplaceReview: true }).populate('reviewer');
+//     res.status(200).json(reviews);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
+
+//Get all reviews 
+router.route("/reviews/").get((req, res) => {
+  Review.find()
+    .then((result) => {
+      res.status(200).json({ result });
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .json({ message: `Feedbacks fetching unsuccessful ${err}` });
+    });
 });
 
 module.exports = router;
